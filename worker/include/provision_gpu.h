6f895d9ee1250b0930bdeb4fab11e4d2eec5a454
Hangchen Yu <yuhc123@gmail.com>
2020-06-30 19:58:24 +0000

    Expose GPU provision APIs to (worker) spec

---
+++ worker/include/provision_gpu.h
@@ -0,0 +1,45 @@
+#ifndef AVA_WORKER_INCLUDE_PROVISION_GPU_H_
+#define AVA_WORKER_INCLUDE_PROVISION_GPU_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint64_t provision_gpu_get_gpu_memory(unsigned gpu_id);
+unsigned provision_gpu_get_gpu_index(unsigned gpu_id);
+
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+#include <memory>
+#include <string>
+#include <vector>
+
+class ProvisionGpu {
+public:
+  ProvisionGpu(std::string& uuid_list, std::string& mem_list);
+  ProvisionGpu(std::vector<std::string>& uuid_vector,
+               std::vector<uint64_t>& mem_vector);
+
+  std::vector<uint64_t> ParseGpuMemoryList(std::string& mem_list);
+  std::vector<std::string> ParseGpuUuidList(std::string& uuid_list);
+
+  uint64_t GetGpuMemory(unsigned gpu_id);
+  unsigned GetGpuIndex(unsigned gpu_id);
+
+private:
+  void Init(std::vector<std::string>& uuid_vector, std::vector<uint64_t>& mem_vector);
+
+  std::vector<unsigned> index_;
+  std::vector<std::string> uuid_;
+  std::vector<uint64_t> memory_;
+};
+
+extern ProvisionGpu* provision_gpu;
+#endif
+
+#endif  // AVA_WORKER_INCLUDE_PROVISION_GPU_H_
---
+++ worker/provision_gpu.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <sstream>
+#include "provision_gpu.h"
+
+ProvisionGpu* provision_gpu;
+
+ProvisionGpu::ProvisionGpu(std::string& uuid_list, std::string& mem_list) {
+  std::vector<std::string> uuid_vector = ParseGpuUuidList(uuid_list);
+  std::vector<uint64_t> mem_vector     = ParseGpuMemoryList(mem_list);
+  Init(uuid_vector, mem_vector);
+}
+
+ProvisionGpu::ProvisionGpu(std::vector<std::string>& uuid_vector,
+                           std::vector<uint64_t>& mem_vector) {
+  Init(uuid_vector, mem_vector);
+}
+
+void ProvisionGpu::Init(std::vector<std::string>& uuid_vector,
+                        std::vector<uint64_t>& mem_vector) {
+  if (uuid_vector.size() != mem_vector.size()) {
+    std::cerr << "Mismatched UUID/Memory vector sizes" << std::endl;
+    exit(1);
+  }
+
+  uuid_ = uuid_vector;
+  memory_ = mem_vector;
+
+  unsigned idx = 0;
+  for (unsigned i = 0; i < uuid_vector.size(); ++i) {
+    bool flag = false;
+    for (unsigned j = 0; j < index_.size(); ++j) {
+      if (uuid_[j] == uuid_[i]) {
+        flag = true;
+        index_.push_back(index_[i]);
+        break;
+      }
+    }
+
+    if (!flag)
+      index_.push_back(idx++);
+  }
+}
+
+std::vector<uint64_t> ProvisionGpu::ParseGpuMemoryList(std::string& mem_list) {
+  std::vector<uint64_t> result;
+  std::stringstream s_stream(mem_list);
+  while (s_stream.good()) {
+    std::string mem;
+    getline(s_stream, mem, ',');
+    if (!mem.empty())
+      result.push_back(std::stol(mem));
+  }
+  return result;
+}
+
+std::vector<std::string> ProvisionGpu::ParseGpuUuidList(std::string& uuid_list) {
+  std::vector<std::string> result;
+  std::stringstream s_stream(uuid_list);
+  while (s_stream.good()) {
+    std::string uuid;
+    getline(s_stream, uuid, ',');
+    if (!uuid.empty())
+      result.push_back(uuid);
+  }
+  return result;
+}
+
+uint64_t ProvisionGpu::GetGpuMemory(unsigned gpu_id) {
+  if (gpu_id < memory_.size())
+    return memory_[gpu_id];
+  else
+    return 0;
+}
+
+unsigned ProvisionGpu::GetGpuIndex(unsigned gpu_id) {
+  if (gpu_id < index_.size())
+    return index_[gpu_id];
+  else
+    return index_.size();  /* Any invalid GPU id. */
+}
+
+uint64_t provision_gpu_get_gpu_memory(unsigned gpu_id) {
+  if (provision_gpu)
+    return provision_gpu->GetGpuMemory(gpu_id);
+  else
+    return 0;
+}
+
+unsigned provision_gpu_get_gpu_index(unsigned gpu_id) {
+  if (provision_gpu)
+    return provision_gpu->GetGpuIndex(gpu_id);
+  else
+    return 0;
+}
