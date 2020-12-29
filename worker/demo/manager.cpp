ebf923310ae5eab83f093cb1919ce5e184442898
Hangchen Yu <hyu@cs.utexas.edu>
2020-12-29 22:00:53 +0000

    Add demo manager

---
+++ worker/demo/manager.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <future>
+#include <iostream>
+#include <thread>
+
+#include "manager_service.h"
+#include "manager_service.pb.h"
+
+using ava_manager::ManagerServiceServerBase;
+
+const uint32_t kDefaultManagerPort    = 3333;
+const uint32_t kDefaultWorkerPortBase = 4000;
+
+class DemoManager : public ManagerServiceServerBase {
+public:
+  DemoManager(uint32_t port, uint32_t worker_port_base, std::string worker_path) :
+    ManagerServiceServerBase(port, worker_port_base, worker_path) {}
+private:
+};
+
+int main(int argc, char* argv[]) {
+  if (argc <= 1) {
+    fprintf(stderr, "Usage: %s <worker_path>\n"
+           "Example: %s generated/cudadrv_nw/worker\n",
+           argv[0], argv[0]);
+    exit(0);
+  }
+  ava_manager::setupSignalHandlers();
+  DemoManager manager(kDefaultManagerPort, kDefaultWorkerPortBase, argv[1]);
+  manager.RunServer();
+  return 0;
+}
