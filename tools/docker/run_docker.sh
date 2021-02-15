ccc6edd79b031cc4ee9ab9ddb14d8cbb2820f0b9
Vance Miller <vance@cs.utexas.edu>
2021-02-15 22:20:52 +0000

    Run ava-cuda or ava-rocm with run_docker.sh

--- tools/docker/run_docker.sh
+++ tools/docker/run_docker.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
-DOCKER_IMAGE=${DOCKER_IMAGE:-ava-rocm}
-DOCKER_NONINTERACTIVE=${DOCKER_NONINTERACTIVE:-0}
+set -e  # exit on error
+
+DOCKER_IMAGE=${DOCKER_IMAGE:-ava-cuda}
+RUN_DOCKER_INTERACTIVE=${RUN_DOCKER_INTERACTIVE:-1}
 
 DEBUG_FLAGS="--cap-add=SYS_PTRACE --security-opt seccomp=unconfined"
 DOCKER_MAP="-v $PWD:$PWD -w $PWD"
-DOCKER_FLAGS="--rm ${DOCKER_MAP} --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined --group-add video ${DEBUG_FLAGS}"
+DOCKER_FLAGS="--rm ${DOCKER_MAP} --ipc=host --security-opt seccomp=unconfined ${DEBUG_FLAGS}"
+if [ ${DOCKER_IMAGE} == "ava-rocm" ]; then
+    DOCKER_FLAGS="${DOCKER_FLAGS} --device=/dev/kfd --device=/dev/dri --group-add video"
+elif [ ${DOCKER_IMAGE} == "ava-cuda" ]; then
+    DOCKER_FLAGS="${DOCKER_FLAGS} --gpus all"
+fi
 # -u `id -u`:`id -g`"
 
-if [ ${DOCKER_NONINTERACTIVE} -eq 0 ]; then
+if [ ${RUN_DOCKER_INTERACTIVE} -eq 1 ]; then
     DOCKER_CMD="docker run -it ${DOCKER_FLAGS} ${DOCKER_IMAGE}"
 else
     DOCKER_CMD="docker run -i ${DOCKER_FLAGS} ${DOCKER_IMAGE}"
