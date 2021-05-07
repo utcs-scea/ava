5d700ad02a1328e5cf69dbb080477278d56f0c7b
Hangchen Yu <hyu@cs.utexas.edu>
2021-05-07 17:14:28 -0500

    Add python format and lint checkers

---
+++ scripts/check_python_format.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PYFMT="${PYFMT:-black} --line-length 120"
+set -eu
+
+if [ $# -eq 0 ]; then
+  echo "$(basename "$0") [-fix] <paths>" >&2
+  exit 1
+fi
+
+FIX=
+if [ "$1" == "-fix" ]; then
+  FIX=1
+  shift 1
+fi
+
+ROOTS=("$@")
+PRUNE_LIST="notebook-home .git build*"
+
+emit_prunes() {
+  for p in $PRUNE_LIST; do echo "-name $p -prune -o"; done | xargs
+}
+
+# shellcheck disable=SC2046,SC2207,SC2038
+FILES=($(find "${ROOTS[@]}" $(emit_prunes) -name '*.py' -print | xargs))
+
+if [ -n "${FIX}" ]; then
+  ${PYFMT} "${FILES[@]}"
+else
+  ${PYFMT} --check "${FILES[@]}"
+fi
