dbe956919255536513f497482820aac7f3b0817f
Hangchen Yu <hyu@cs.utexas.edu>
2021-05-07 19:15:19 -0500

    Fix lint dependency version

--- scripts/setup_lint_dependencies.sh
+++ scripts/setup_lint_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt install python3 python3-pip
-python3 -m pip install pip
-python3 -m pip install 'clang-format==9.0.0' 'black==21.5b0' 'pylint-2.8.2'
+python3 -m pip install --upgrade pip
+python3 -m pip install setuptools
+python3 -m pip install 'clang-format==9.0.0' 'black==21.5b0' 'pylint==2.8.2'
