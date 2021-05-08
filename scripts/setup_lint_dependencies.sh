e51da23a0616a88c55ae6bf202aa5d8503960d9c
Hangchen Yu <hyu@cs.utexas.edu>
2021-05-07 20:41:56 -0500

    Use shellcheck version 0.7.2.1

--- scripts/setup_lint_dependencies.sh
+++ scripts/setup_lint_dependencies.sh
@@ -3,4 +3,4 @@
 sudo apt install python3 python3-pip
 python3 -m pip install --upgrade pip
 python3 -m pip install setuptools
-python3 -m pip install 'clang-format==9.0.0' 'black==21.5b0' 'pylint==2.8.2' 'shellcheck-py==0.7.2'
+python3 -m pip install 'clang-format==9.0.0' 'black==21.5b0' 'pylint==2.8.2' 'shellcheck-py==0.7.2.1'
