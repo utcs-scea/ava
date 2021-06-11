3bbfdcb7795c597ef1e34361de8a45904dc02bed
Hangchen Yu <athy@fb.com>
2021-06-11 00:05:39 -0500

    Enable format and lint checks for CAvA Python code

--- scripts/check_python_lint.sh
+++ scripts/check_python_lint.sh
@@ -14,8 +14,8 @@ LINT=${PYLINT:-pylint}
 ARGS="-j 0 --rcfile=${GIT_ROOT}/.pylintrc --suggestion-mode=n"
 ROOTS=("$@")
 FAILED=
-PRUNE_PATHS="cava/nightwatch llvm third_party"
-PRUNE_NAMES=".git build* nightwatch* third_party*"
+PRUNE_PATHS="llvm third_party"
+PRUNE_NAMES=".git build* third_party*"
 TEST_DIRS="${GIT_ROOT}/tests/"
 # lint warnings that are useless for tests since tests are run for ci anyway
 # eliminate unused-argument since it is triggered by pytest harnesses whose value is not needed.
