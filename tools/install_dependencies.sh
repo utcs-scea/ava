148d2bb98b7827a2629d339ba3403c4c28af4631
Vance Miller <vancemiller@users.noreply.github.com>
2021-06-15 19:54:48 -0400

    Install wget dependencies (#165)

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -11,12 +11,13 @@ if [[ $(lsb_release -rs) != "18.04" ]]; then
   fi
 fi
 
+sudo apt install -y apt-transport-https ca-certificates gnupg software-properties-common wget
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
 sudo apt update
 sudo apt purge --auto-remove cmake
-sudo apt install cmake cmake-curses-gui
-sudo apt install git build-essential python3 python3-pip libglib2.0-dev clang-7 libclang-7-dev \
+sudo apt install -y cmake cmake-curses-gui
+sudo apt install -y git build-essential python3 python3-pip libglib2.0-dev clang-7 libclang-7-dev \
   indent ninja-build
 python3 -m pip install pip
 python3 -m pip install setuptools pkgconfig
