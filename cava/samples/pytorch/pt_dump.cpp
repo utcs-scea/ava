b27a25a998a4188306e6081697910ae9a35a720d
Zhiting Zhu <zhitingz@cs.utexas.edu>
2021-06-25 07:01:32 +0000

    use ava_unsupported
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- cava/samples/pytorch/pt_dump.cpp
+++ cava/samples/pytorch/pt_dump.cpp
@@ -1316,14 +1316,12 @@ cublasStatus_t CUBLASWINAPI cublasGetVector(int n, int elemSize, const void *x, 
 
 cublasStatus_t CUBLASWINAPI cublasSetVectorAsync(int n, int elemSize, const void *hostPtr, int incx, void *devicePtr,
                                                  int incy, cudaStream_t stream) {
-  fprintf(stderr, "%s is not implemented\n", __func__);
-  abort();
+  ava_unsupported;
 }
 
 cublasStatus_t CUBLASWINAPI cublasGetVectorAsync(int n, int elemSize, const void *devicePtr, int incx, void *hostPtr,
                                                  int incy, cudaStream_t stream) {
-  fprintf(stderr, "%s is not implemented\n", __func__);
-  abort();
+  ava_unsupported;
 }
 
 CUBLASAPI cublasStatus_t CUBLASWINAPI cublasGetMathMode(cublasHandle_t handle, cublasMath_t *mode) { ava_unsupported; }
@@ -11342,6 +11340,5 @@ __host__ cudaError_t CUDARTAPI cudaProfilerStop(void);
 __host__ cudaError_t CUDARTAPI cudaThreadSynchronize(void) {}
 
 __host__ cudaError_t CUDARTAPI cudaGetExportTable(const void **ppExportTable, const cudaUUID_t *pExportTableId) {
-  fprintf(stderr, "%s is not implemented\n", __func__);
-  abort();
+  ava_unsupported;
 }
